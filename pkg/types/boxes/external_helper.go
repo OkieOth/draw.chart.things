58e8f1434a111c97a53d82e22ae4132f969e0eac
Eiko Thomas <13103714+OkieOth@users.noreply.github.com>
2026-01-14 23:00:55 +0100

    wip: add functionality, but entry functions are still missing

---
+++ pkg/types/boxes/external_helper.go
@@ -0,0 +1,48 @@
+package boxes
+
+import (
+	"maps"
+	"slices"
+)
+
+func (b *Boxes) MixinFormats(additional *AdditionalFormats) {
+	if additional == nil {
+		return
+	}
+	if len(additional.Formats) > 0 {
+		maps.Copy(b.Formats, additional.Formats)
+	}
+	for _, i := range additional.Images {
+		b.Images = append(b.Images, i)
+	}
+}
+
+func hasConnection(connections []Connection, destId string) bool {
+	return slices.ContainsFunc(connections, func(c Connection) bool {
+		return c.DestId == destId
+	})
+}
+
+func (b *Boxes) mixInConnectionsImplCont(cont []Layout, additional map[string]ConnectionCont) {
+	for i := range len(cont) {
+		b.mixInConnectionsImpl(&cont[i], additional)
+	}
+}
+
+func (b *Boxes) mixInConnectionsImpl(l *Layout, additional map[string]ConnectionCont) {
+	if l.Id != "" {
+		if cc, ok := additional[l.Id]; ok {
+			for _, c := range cc.Connections {
+				if !hasConnection(l.Connections, c.DestId) {
+					l.Connections = append(l.Connections, c)
+				}
+			}
+		}
+	}
+	b.mixInConnectionsImplCont(l.Horizontal, additional)
+	b.mixInConnectionsImplCont(l.Vertical, additional)
+}
+
+func (b *Boxes) MixinConnections(additional map[string]ConnectionCont) {
+	b.mixInConnectionsImpl(&b.Boxes, additional)
+}
---
+++ pkg/types/boxes/externals.go
@@ -0,0 +1,65 @@
+package boxes
+
+// Attention, this file is generated. Manual changes get lost with the next
+// run of the code generation.
+// created by yacg (template: golang_types.mako v1.1.0)
+
+import (
+	"github.com/okieoth/draw.chart.things/pkg/types"
+)
+
+type ConnectionCont struct {
+	Connections []Connection `yaml:"connections,omitempty"`
+}
+
+func NewConnectionCont() *ConnectionCont {
+	return &ConnectionCont{
+		Connections: make([]Connection, 0),
+	}
+}
+
+func CopyConnectionCont(src *ConnectionCont) *ConnectionCont {
+	if src == nil {
+		return nil
+	}
+	var ret ConnectionCont
+	ret.Connections = make([]Connection, 0)
+	for _, e := range src.Connections {
+		ret.Connections = append(ret.Connections, e)
+	}
+
+	return &ret
+}
+
+/* Model to inject additional formats in a boxes layout definition
+ */
+type AdditionalFormats struct {
+	Formats map[string]Format `yaml:"formats,omitempty"`
+
+	// optional list of images used in the generated graphic
+	Images []types.ImageDef `yaml:"images,omitempty"`
+}
+
+func NewAdditionalFormats() *AdditionalFormats {
+	return &AdditionalFormats{
+		Formats: make(map[string]Format, 0),
+		Images:  make([]types.ImageDef, 0),
+	}
+}
+
+func CopyAdditionalFormats(src *AdditionalFormats) *AdditionalFormats {
+	if src == nil {
+		return nil
+	}
+	var ret AdditionalFormats
+	ret.Formats = make(map[string]Format, 0)
+	for k, v := range src.Formats {
+		ret.Formats[k] = v
+	}
+	ret.Images = make([]types.ImageDef, 0)
+	for _, e := range src.Images {
+		ret.Images = append(ret.Images, e)
+	}
+
+	return &ret
+}
